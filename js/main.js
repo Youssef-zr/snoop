ec555ff1095bbc5c506b93f93fe06e75b63065a6
Youssef-zr <yn-neinaa@hotmail.com>
2021-08-07 01:47:39 +0100

    footer

--- js/main.js
+++ js/main.js
@@ -20,9 +20,10 @@ $(() => {
     let nav_items = $('#section-header').find(".nav-item .nav-link").not('.not-link');
     nav_items.click(function (e) {
         e.preventDefault();
-
+        // alert('gd')
+        $(this).addClass('active').parent('li').siblings().find('a').removeClass('active')
         $('html,body').animate({
-            scrollTop: ($($(this).data('section')).offset().top) - $('nav').outerHeight()-30
+            scrollTop: ($($(this).data('section')).offset().top) - $('nav').outerHeight() - 30
         }, 700)
     })
 
@@ -60,7 +61,7 @@ $(() => {
         for (i = 0; i < nav_items.length; i++) {
             let $nav_item = $(nav_items[i]);
 
-            if ($('html,body').scrollTop() >= ($($nav_item.data('section')).offset().top)-$('nav').outerHeight() - 60) {
+            if ($('html,body').scrollTop() >= ($($nav_item.data('section')).offset().top) - $('nav').outerHeight() - 40) {
                 $nav_item.addClass('active').parent('li').siblings().find('a').removeClass('active')
             }
         }
